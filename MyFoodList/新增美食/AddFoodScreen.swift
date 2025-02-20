d473728230b2b72889e220eb96304cb51ca57df9
Johnny <zhan0619@gmail.com>
2025-02-20 10:52:12 +0800

    新增AddFoodScreen

---
+++ MyFoodList/新增美食/AddFoodScreen.swift
@@ -0,0 +1,24 @@
+//
+//  AddFoodScreen.swift
+//  MyFoodList
+//
+//  Created by 紹郁 on 2025/2/20.
+//
+
+import UIKit
+
+class AddFoodScreen: MyViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+}
+
+//MARK: - UI
+extension AddFoodScreen {
+    func setupUI() {
+        
+    }
+}
