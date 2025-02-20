9a580bf98024a820c29f9bb309bbfb4f73a14cac
Johnny <zhan0619@gmail.com>
2025-02-20 10:37:20 +0800

    Initial Commit

---
+++ MyFoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFoodList
+//
+//  Created by 紹郁 on 2025/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
